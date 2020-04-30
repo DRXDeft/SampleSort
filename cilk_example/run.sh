87bd5662fe9f5207b6992c8b8e3b9fb5c8050e60
Rumble <wangzhongqi96@hotmail.com>
2020-04-29 19:16:43 -0700

    Update run.sh

--- cilk_example/run.sh
+++ cilk_example/run.sh
@@ -3,4 +3,3 @@ scl enable devtoolset-7 bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/src/tbb/build/linux_intel64_gcc_cc7.3.1_libc2.17_kernel3.10.0_release/
 make clean
 make -f makefile
-echo miao
