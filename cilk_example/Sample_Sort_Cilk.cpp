044f7d6a551c32c300cf8c24e4a40a0765b66407
Rumble <wangzhongqi96@hotmail.com>
2020-05-01 15:36:35 -0700

    Update Sample_Sort_Cilk.cpp

--- cilk_example/Sample_Sort_Cilk.cpp
+++ cilk_example/Sample_Sort_Cilk.cpp
@@ -341,7 +341,7 @@ int main(int argc, char** argv) {
     else if (distribution ==6)
         normal(A,n);
     for (int i = 0; i < n; i++) C[i] = 0;
-	cilk_for (int i = 0; i < n; i++) A[i] = j;
+	cilk_for (int i = 0; i < n; i++) A[i] = n-i;
     //Verification(A,n);
     cout << "Data Generation is Complete, Start Sorting and Timing!\n";
 	timer t; t.start();
