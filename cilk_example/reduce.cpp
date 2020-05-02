c71acd0509641bf8a45282570d012925046b4280
Rumble <wangzhongqi96@hotmail.com>
2020-05-01 21:09:16 -0700

    Update reduce.cpp

--- cilk_example/reduce.cpp
+++ cilk_example/reduce.cpp
@@ -1,48 +1,71 @@
 #include <iostream>
 #include <cstdio>
-#include <stdlib.h>
+#include <cstdlib>
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
+#include <cstring>
+#include <fstream>
 #include "get_time.h"
 using namespace std;
+ofstream fout;
+int n,leaves,bottom,nodes;
+void Reduce_Size(int offset , int n, long long* A, long long* PrefixSum ){
+	
+	PrefixSum[offset] = A[offset];
 
-int th = 1;
+	if (n==1){	return;	}
 
-int reduce(int* A, int n) {
-	if (n <= th) {
-		int ret = 0;
-		for (int i = 0; i < n; i++) {
-			ret += A[i];
-		}
-		return ret;
+	cilk_for (int i = 0; i < n/2; i++)
+		A[i+offset+n] = A[offset + 2*i] + A[offset+1 +2*i];
+	Reduce_Size(offset + n, n/2, A , PrefixSum);
+	cilk_for (int i = 1; i< n; i++){
+		if (i%2)
+			PrefixSum[offset+i] = PrefixSum[offset+n+i/2];
+		else
+			PrefixSum[offset+i] = PrefixSum[offset+n+i/2-1]+A[offset+i];
 	}
-	int L, R;
-	L = cilk_spawn reduce(A, n/2);
-	R = reduce(A+n/2, n-n/2);
-	cilk_sync;
-	return L+R;
+	//----------------------------------------------------------------------------------------
+	int j = offset + n, i=offset;
+	cilk_for (int lIndex=0; lIndex<n/2; lIndex++){
+		A[j] = A[i]+A[i+1];
+		//fout <<"A["<<j<<"]=A["<<i<<"]+A["<<i+1<<"] = "<<A[j]<<" "<<A[i]<<" "<<A[i+1]<<endl;
+		i+=2;
+		j++;
+	}
+	Reduce_Size(offset + n, n/2, A , PrefixSum);
+	j = offset + n;
+	int lIndex=1;
+	cilk_for (int k = offset+1; k<offset+n; k++){
+		if (lIndex%2)
+			PrefixSum[k] = PrefixSum[j];
+		else
+			PrefixSum[k] = PrefixSum[j++]+A[k];
+		lIndex++;
+	}
+	
 }
-
-int main(int argc, char** argv) {
-	if (argc < 2) {
-		cout << "Usage: ./reduce [num_elements] [threshold=1]" << endl;
+int main(int argc, char** argv){
+    if (argc != 2) {
+		cout << "Usage: ./scan [num_elements]" << endl;
 		return 0;
 	}
-	int n = atoi(argv[1]);
-	if (argc == 3) {
-		th = atoi(argv[2]);}
-	int* A = new int[n];
-	cilk_for (int i = 0; i < n; i++) A[i] = i;
-	timer t0; t0.start();
-	int ans = 0;
-	for (int i = 0; i < n; i++) ans += A[i];
-	t0.stop();
-	cout << ans << ". sequential running time = " << t0.get_total() << endl;
-	timer t; t.start();
-	int x = reduce(A, n);
-	t.stop();
-	cout << "time: " << t.get_total() << endl;
-	cout << x << endl;
+	n = atoi(argv[1]);
 	
-	return 0;
+	long long* A = new long long[3 * n];
+	long long* PrefixSum = new long long[3*n];
+	
+	cilk_for (int i = 0; i<n; i++)
+		A[i] = i;
+	//fout.open("prefixsum.txt");
+	timer t; 
+	t.start();
+	Reduce_Size(0,n,A,PrefixSum);
+	t.stop();
+	//fout.open("prefixsum.txt");
+	//for (int i = 0; i < 2*n; i++) cout <<i<<" " <<PrefixSum[i] <<endl;
+	//fout.close();
+	cout<<PrefixSum[0]<<" "<<PrefixSum[n/2]<<" "<<PrefixSum[n-1]<<endl;
+	cout << "time: " <<fixed<<setprecision(4)<< t.get_total() << endl;
+
+    return 0;
 }
